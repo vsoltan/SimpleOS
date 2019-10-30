26fc9acc394658b38d1e0a45f4baf9335c8ed19f
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-10-30 18:49:48 -0400

    added basic functionality to toggle display on and off

---
+++ playground/playground.ino
@@ -0,0 +1,61 @@
+
+#include <Adafruit_SSD1306.h>
+
+#define UIScreenWidth 128
+#define UIScreenHeight 64
+#define SerialRate 9600
+#define DEBOUNCE 300
+
+#define POWERBUTTON 0
+
+Adafruit_SSD1306 lcd(UIScreenWidth, UIScreenHeight);
+
+void initializeDisplay() {
+  lcd.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  lcd.setTextColor(WHITE);
+  lcd.setTextSize(2); // use larger font
+  lcd.setCursor(0,0); // set cursor to pixel (0,0)
+  lcd.clearDisplay();
+}
+
+void initializePeripherals() {
+  pinMode(POWERBUTTON, INPUT_PULLUP);
+}
+
+void sleepDisplay(Adafruit_SSD1306* display) {
+  display->ssd1306_command(SSD1306_DISPLAYOFF);
+}
+
+void wakeDisplay(Adafruit_SSD1306* display) {
+  display->ssd1306_command(SSD1306_DISPLAYON);
+}
+
+byte displayOn = 0;
+
+void togglePower() {
+  if (displayOn) {
+    sleepDisplay(&lcd);
+   } else {
+    wakeDisplay(&lcd);
+   }
+   displayOn = !displayOn;
+}
+
+void setup() {
+  Serial.begin(SerialRate);
+  initializeDisplay();
+  initializePeripherals();
+  lcd.println("Welcome to \n dumbOS");
+  lcd.display();
+}
+
+long lastButtonPress = 0;
+byte prev_state = 1;
+
+void loop() {
+  byte curr_state = digitalRead(POWERBUTTON);
+  if (curr_state == 0 && prev_state == 1 && millis() >= lastButtonPress + DEBOUNCE) {
+    togglePower();
+    lastButtonPress = millis();
+  }
+}
