2faafd31dfcc0615e27ace3a4490730bc1117a98
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-11-30 16:37:24 -0500

    removed lcd from struct

--- os/os.ino
+++ os/os.ino
@@ -1,23 +1,45 @@
+
 #include "display.h"
 #include "buttons.h"
 
 #define BAUD 115200
 
-display * watchDisplay;
+Adafruit_SSD1306 lcd(UIScreenWidth, UIScreenHeight);
+DisplayStatus * displayStatus;
 
 void setup() {
     Serial.begin(BAUD);
-    watchDisplay = createDisplay();
+    initializeDisplay(&lcd);
+    displayStatus = createDisplayStatus();
+    // initializePeripherals();
 }
+
+// unsigned long lastclick_time = 0;
+
+/*
+ * TODO: tried to convert to an interrupt but it didn't work
+ */
+// void IRAM_ATTR power() {
+//     if (millis() > lastclick_time + DEBOUNCE) {
+//         Serial.println("pressed");
+//         togglePower(displayStatus, &lcd);
+//         lastclick_time = millis();
+//     }
+// }
+//
+// void initializePeripherals() {
+//     pinMode(POWERBUTTON, INPUT_PULLUP);
+//     attachInterrupt(POWERBUTTON, power, FALLING);
+// }
 
 long lastButtonPress = 0;
 byte prev_state = 1;
 
 void loop() {
-  byte curr_state = digitalRead(POWERBUTTON);
-  if (curr_state == 0 && prev_state == 1 && millis() >= lastButtonPress + DEBOUNCE) {
-    togglePower(watchDisplay);
-    lastButtonPress = millis();
-  }
-  prev_state = curr_state;
+    byte curr_state = digitalRead(POWERBUTTON);
+    if (curr_state == 0 && prev_state == 1 && millis() >= lastButtonPress + DEBOUNCE) {
+        togglePower(displayStatus, &lcd);
+        lastButtonPress = millis();
+    }
+    prev_state = curr_state;
 }
