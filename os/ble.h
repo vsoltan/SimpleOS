aecfa61de325b6d0115c82a79ee63b6b93bbffc6
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-12-03 23:30:36 -0500

    removed irrelevant code

--- os/ble.h
+++ os/ble.h
@@ -4,10 +4,7 @@
 #include <BLE2902.h>
 
 // SERVICES
-#define BatteryService BLEUUID((uint16_t)0x180F)
 
 // CHARACTERISTICS
-BLECharacteristic BatteryLevelCharacteristic(BLEUUID((uint16_t)0x2A19), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
 
 // DESCRIPTORS
-BLEDescriptor BatteryLevelDescriptor(BLEUUID((uint16_t)0x2901));
